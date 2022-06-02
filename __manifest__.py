b10fe861d1a0be0399373e281c31564796088fa1
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2022-06-02 17:22:09 -0500

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+
+{
+	'name': 'Migración Adjuntos - Colombia',
+	
+	'version': '14.0',
+       
+	'summary': 'Migración V1 - Colombia',
+	'description': """
+Plantillas de Migración Colombia:
+======================
+	* Este módulo realiza una migracion por URL.
+	
+	""",
+	'depends': [
+		
+		'account',
+	],
+	'data': [
+		
+		
+	],
+	'installable': True,
+	'application': True,
+	'auto_install': True,
+}
